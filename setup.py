9fd44ee9776bd813e15a890f682bead3c1b335e4
Stanislav Zmiev <zertarx@gmail.com>
2019-12-25 10:50:45 -0500

    Add distribution

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+requires = [
+    'unischeduler',
+    'flask'
+]
+
+setup(
+    name="unischeduler_web",
+    version="0.1",
+    packages=['unischeduler_web'],
+    install_requires=requires,
+
+    # metadata to display on PyPI
+    author="Stanislav Zmiev",
+    author_email="szmiev2000@gmail.com",
+    description="Web interface for unischeduler",
+    license="MIT",
+    project_urls={"Source Code": "https://github.com/Varabe/unischeduler_web"},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
