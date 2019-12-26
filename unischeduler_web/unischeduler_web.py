6091ded958e4bc5cf727c5edb4dd239dce164cb0
Stanislav Zmiev <zertarx@gmail.com>
2019-12-25 22:18:43 -0500

    Add unknown exception handling

--- unischeduler_web/unischeduler_web.py
+++ unischeduler_web/unischeduler_web.py
@@ -1,6 +1,8 @@
 from flask import Flask, send_file, render_template, request
 import io
 import unischeduler
+from traceback import print_exception
+import sys
 
 
 app = Flask(__name__)
@@ -14,13 +16,21 @@ def main_page():
 @app.route('/make_ical/')
 def make_ical():
     try:
+        schedule = request.args.get("schedule")
+        isUCF = bool(request.args.get("isUCF"))
         return send_file(
-            io.BytesIO(unischeduler.main(request.args.get("schedule"), bool(request.args.get("isUCF")))),
+            io.BytesIO(unischeduler.main(schedule, isUCF)),
             mimetype="text/calendar",
             as_attachment=True,
             attachment_filename="Classes.ics")
     except unischeduler.util.SchedulerError as e:
         return str(e)
+    except Exception as e:
+        with open("log", 'a') as f:
+                print(e)
+                f.write(f"\nisUCF={isUCF}\n{schedule}\n")
+                print_exception(*sys.exc_info, file=f)
+        return "An unknown error occured. Contact my developer and he'll fix it ASAP."
 
 
 @app.route('/guide/')
