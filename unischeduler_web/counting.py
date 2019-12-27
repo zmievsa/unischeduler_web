92ea059d4b25bf2d6aa9c66edc75607fa26ad110
Stanislav Zmiev <zertarx@gmail.com>
2019-12-26 19:52:19 -0500

    Add counting app uses

---
+++ unischeduler_web/counting.py
@@ -0,0 +1,25 @@
+from typing import Dict
+from configparser import ConfigParser
+from multiprocessing import Value
+from pathlib import Path
+
+
+def retrieve_counters(path_to_db, counter_names) -> Dict[str, Value]:
+    config = ConfigParser()
+    path = Path(path_to_db)
+    if not path.exists:
+        path.touch()
+    config.read(path_to_db)
+    return {name:Value('i', config.getint("DEFAULT", name, fallback=0)) for name in counter_names}
+
+
+def save_counters(path_to_db, counters: dict):
+    counters = {name:counters[name].value for name in counters}
+    config = ConfigParser(counters)
+    with open(path_to_db, "w") as f:
+        config.write(f)
+
+
+def increment(counter: Value):
+    with counter.get_lock():
+        counter.value += 1
--- unischeduler_web/unischeduler_web.py
+++ unischeduler_web/unischeduler_web.py
@@ -1,12 +1,22 @@
+# TODO: Add logging
+
 from flask import Flask, send_file, render_template, request
 import io
 import unischeduler
 from traceback import print_exception
 import sys
+import atexit
+from counting import retrieve_counters, save_counters, increment
+from pathlib import Path 
+
+CURRENT_DIR = Path(__file__).parent
+PATH_TO_COUNTER_DB = CURRENT_DIR / "counters.ini"
+COUNTER_NAMES = ("total_uses", "successful_use", "unknown_error")
 
 
 app = Flask(__name__)
-# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+counters = retrieve_counters(PATH_TO_COUNTER_DB, COUNTER_NAMES)
+atexit.register(save_counters, PATH_TO_COUNTER_DB, counters)
 
 @app.route('/')
 def main_page():
@@ -15,10 +25,11 @@ def main_page():
 
 @app.route('/make_ical/')
 def make_ical():
+    increment(counters['total_uses'])
     try:
         schedule = request.args.get("schedule")
         isUCF = bool(request.args.get("isUCF"))
-        return send_file(
+        response = send_file(
             io.BytesIO(unischeduler.main(schedule, isUCF)),
             mimetype="text/calendar",
             as_attachment=True,
@@ -26,11 +37,14 @@ def make_ical():
     except unischeduler.util.SchedulerError as e:
         return str(e)
     except Exception as e:
-        with open("log", 'a') as f:
-                print(e)
-                f.write(f"\nisUCF={isUCF}\n{schedule}\n")
+        increment(counters['unknown_error'])
+        with open(CURRENT_DIR / "log", 'a') as f:
+                print(f"Unknown error: {e}")
                 print_exception(*sys.exc_info(), file=f)
         return "An unknown error occured. Contact my developer and he'll fix it ASAP."
+    else:
+        increment(counters['successful_use'])
+        return response
 
 
 @app.route('/guide/')
