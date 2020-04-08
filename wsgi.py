722c9c0037b164163919c52bc3e5ddc14b5d9dd4
Stanislav Zmiev <zertarx@gmail.com>
2020-04-07 21:22:27 -0400

    Disable accidental debug mode

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from unischeduler_web.main import app
 import sys
 
 
-app.config['DEBUG'] = True
+app.config['DEBUG'] = False
 
 if __name__ == "__main__":
     app.run(port=5000 if len(sys.argv) < 2 else int(sys.argv[1]))
