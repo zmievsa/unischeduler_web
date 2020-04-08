5f0093a44cb8fbdc677266584a3a9cccbede56db
Stanislav Zmiev <zertarx@gmail.com>
2020-04-07 23:18:17 -0400

    Add proper cli args to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,16 @@
 from unischeduler_web.main import app
 import sys
 
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        debug = sys.argv[2].lower() == "debug"
+    else:
+        debug = False
+else:
+    port = 5000
 
-app.config['DEBUG'] = False
+app.config['DEBUG'] = debug
 
 if __name__ == "__main__":
-    app.run(port=5000 if len(sys.argv) < 2 else int(sys.argv[1]))
+    app.run(port=port)
