1ee5a74274007326e7c7bff8cf11fd7d7b2a415e
Stanislav Zmiev <szmiev2000@gmail.com>
2020-08-22 03:44:47 -0400

    Fix deployment and wsgi not working in production

--- deploy.sh
+++ deploy.sh
@@ -1,46 +1,59 @@
-#!/bin/bash
-# Must be logged in as non-root sudo user
+#!/bin/sh
+set -e
+if [ "$EUID" -eq 0 ]
+  then echo "Please, do not run me as root."
+  exit 1
+fi
+
+PROJDIR=unischeduler_web
+PROJDIRPATH=$(dirname $(realpath $0))
+DNSNAME=scheduler.oatmeal.cc
+
 sudo apt update
-sudo apt install nginx python3 python3-pip python3-dev ufw git
+sudo apt install nginx python3 python3-pip python3-dev ufw git certbot python-certbot-nginx
 # Configure firewall
 sudo ufw allow OpenSSH
 sudo ufw allow 'Nginx Full'
 sudo ufw enable
-pip3 install --user pipenv
-cd ~
-# # To use python packages from console
-echo "PYTHON_BIN_PATH='$(python3 -m site --user-base)/bin'
-PATH='$PATH:$PYTHON_BIN_PATH'" >> .bashrc 
-# Source .bashrc won't work in a script
 
-cd unischeduler_web
+pip3 install pipenv
+# To use python packages from console
+# (Source .bashrc won't work in a script)
+echo "PYTHON_BIN_PATH='$(python3 -m site --user-base)/bin'
+PATH='$PATH:$PYTHON_BIN_PATH'" >> ~/.bashrc 
+
+cd $PROJDIRPATH
 mkdir .venv # Sets pipenv to put everything in .venv
+echo "If an error happens now, it means you have some problems with your project. Check if it runs by itself"
 python3 -m pipenv install -e . # install from setup.py
 cd ..
+echo "Now we will add all the necessary configurations"
 # gunicorn --preload means that we load our app before launching workers. This makes workers share multiprocessing locks
 echo "[Unit]
-Description=Gunicorn instance to serve unischeduler_web
+Description=Gunicorn instance to serve $PROJDIR
 After=network.target
 
 [Service]
 User=$USER
 Group=www-data
-WorkingDirectory=/home/$USER/unischeduler_web
-Environment="PATH=/home/$USER/unischeduler_web/.venv/bin"
-ExecStart=/home/$USER/unischeduler_web/.venv/bin/gunicorn --preload --workers 3 --bind unix:unischeduler_web.sock -m 007 wsgi:app
+WorkingDirectory=$PROJDIRPATH
+Environment="PATH=$PROJDIRPATH/.venv/bin"
+Environment="DEBUG=0"
+ExecStart=$PROJDIRPATH/.venv/bin/gunicorn --preload --workers 3 --bind unix:$PROJDIR.sock -m 007 wsgi:app
 
 [Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/unischeduler_web.service # Create service to run app on startup
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/$PROJDIR.service # Create service to run app on startup
 echo "server {
     listen 443 ssl;
     listen [::]:443 ssl;
-    ssl_certificate /etc/letsencrypt/live/scheduler.oatmeal.cc/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/scheduler.oatmeal.cc/privkey.pem;
-    server_name scheduler.oatmeal.cc;
+    # Use these two lines if you accidentally lose your configuration
+    # ssl_certificate /etc/letsencrypt/live/$DNSNAME/fullchain.pem;
+    # ssl_certificate_key /etc/letsencrypt/live/$DNSNAME/privkey.pem;
+    server_name $DNSNAME;
 
     location / {
         include proxy_params;
-        proxy_pass http://unix:/home/$USER/unischeduler_web/unischeduler_web.sock;
+        proxy_pass http://unix:$PROJDIRPATH/$PROJDIR.sock;
     }
 
 }
@@ -48,12 +61,18 @@ server {
     listen 80;
     listen [::]:80;
 
-    server_name scheduler.oatmeal.cc;
+    server_name $DNSNAME;
 
     return 302 https://$server_name$request_uri;
-}" | sudo tee /etc/nginx/sites-available/unischeduler_web # Nginx will catch all requests and forward them to unischeduler_web
-sudo ln -s /etc/nginx/sites-available/unischeduler_web /etc/nginx/sites-enabled
+}" | sudo tee /etc/nginx/sites-available/$PROJDIR # Nginx will catch all requests and forward them to $PROJDIR
+sudo ln -s /etc/nginx/sites-available/$PROJDIR /etc/nginx/sites-enabled
 sudo rm /etc/nginx/sites-enabled/default
-sudo systemctl start unischeduler_web
-sudo systemctl enable unischeduler_web
+
+sudo certbot --nginx
+
+sudo systemctl start $PROJDIR
+sudo systemctl enable $PROJDIR
 sudo systemctl restart nginx
+
+echo "alias restart-server=\"sudo systemctl restart $PROJDIR\"" >> ~/.bashrc
+echo "You can use restart-server command to push updates"
--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,22 @@
 from unischeduler_web.main import app
 import sys
+import os
 
-if len(sys.argv) > 1:
-    port = int(sys.argv[1])
-    if len(sys.argv) > 2:
-        debug = sys.argv[2].lower() == "debug"
+
+# You can't rely on argv if an external process like gunicorn runs the server
+# because the args are going to be from the call to gunicorn, not unischeduler.
+# So don't be stupid and don't change this module without double-checking.
+
+# 1 = True, Anything else = false
+debug = os.environ.get('DEBUG', "1") == "1"
+app.config['DEBUG'] = debug
+if debug:
+    if len(sys.argv) < 2:
+        port = 8000
     else:
-        debug = False
+        port = int(sys.argv[1])
 else:
     port = 5000
-
-app.config['DEBUG'] = debug
 
 if __name__ == "__main__":
     app.run(port=port)
